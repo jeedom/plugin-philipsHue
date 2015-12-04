f782357e69fc52e458bc15635fa07bf84366b33b
Loïc <loic@jeedom.com>
2015-12-04 20:42:26 +0100

    Try fix

--- desktop/php/philipsHue.php
+++ desktop/php/philipsHue.php
@@ -25,12 +25,6 @@ foreach ($eqLogics as $eqLogic) {
    <legend>{{Mes Philips Hue}}
    </legend>
    <div class="eqLogicThumbnailContainer">
-     <div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
-      <center>
-       <i class="fa fa-plus-circle" style="font-size : 7em;color:#94ca02;"></i>
-     </center>
-     <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02"><center>Ajouter</center></span>
-   </div>
    <?php
 foreach ($eqLogics as $eqLogic) {
 	$opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');

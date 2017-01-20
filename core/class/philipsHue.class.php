58a8260cee78676ad51434425d799842aee54486
Loïc <loic@jeedom.com>
2017-01-20 16:12:30 +0100

    cleaning

--- core/class/philipsHue.class.php
+++ core/class/philipsHue.class.php
@@ -314,61 +314,6 @@ class philipsHue extends eqLogic {
 		}
 	}
 
-	/*****************************COLOR CONVERTION**************************************/
-
-	public static function xyBriToRgb($x, $y, $bri) {
-		$hex_RGB = "";
-		$hex_value = "";
-		if (isset($x) && isset($y) && isset($bri) && $y != 0) {
-			$z = 1.0 - $x - $y;
-			$Y = $bri / 255;
-			$X = ($Y / $y) * $x;
-			$Z = ($Y / $y) * $z;
-			$r = $X * 1.612 - $Y * 0.203 - $Z * 0.302;
-			$g = -$X * 0.509 + $Y * 1.412 + $Z * 0.066;
-			$b = $X * 0.026 - $Y * 0.072 + $Z * 0.962;
-			$r = ($r <= 0.0031308) ? 12.92 * $r : (1.0 + 0.055) * pow($r, (1.0 / 2.4)) - 0.055;
-			$g = ($g <= 0.0031308) ? 12.92 * $g : (1.0 + 0.055) * pow($g, (1.0 / 2.4)) - 0.055;
-			$b = ($b <= 0.0031308) ? 12.92 * $b : (1.0 + 0.055) * pow($b, (1.0 / 2.4)) - 0.055;
-			$maxValue = max($r, $g, $b);
-			if ($maxValue > 0) {
-				$r /= $maxValue;
-				$g /= $maxValue;
-				$b /= $maxValue;
-			}
-			$r = $r * 255;
-			if ($r < 0) {
-				$r = 255;
-			}
-			$g = $g * 255;
-			if ($g < 0) {
-				$g = 255;
-			}
-			$b = $b * 255;
-			if ($b < 0) {
-				$b = 255;
-			}
-			$hex_value = dechex($r);
-			if (strlen($hex_value) < 2) {
-				$hex_value = "0" . $hex_value;
-			}
-			$hex_RGB .= $hex_value;
-			$hex_value = dechex($g);
-			if (strlen($hex_value) < 2) {
-				$hex_value = "0" . $hex_value;
-			}
-			$hex_RGB .= $hex_value;
-			$hex_value = dechex($b);
-			if (strlen($hex_value) < 2) {
-				$hex_value = "0" . $hex_value;
-			}
-			$hex_RGB .= $hex_value;
-			return "#" . $hex_RGB;
-		} else {
-			return '#FFFFFF';
-		}
-	}
-
 	/*     * *********************Méthodes d'instance************************* */
 
 	public function preInsert() {

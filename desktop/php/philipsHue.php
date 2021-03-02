79aeaf17827a0ef79a0c7e7611550a4595187005
Salvialf <jeuxalf@live.fr>
2021-03-02 23:57:05 +0100

    general enhancements (desktop/images)

--- desktop/php/philipsHue.php
+++ desktop/php/philipsHue.php
@@ -27,8 +27,13 @@ $eqLogics = eqLogic::byType($plugin->getId());
 				<span>{{Synchroniser}}</span>
 			</div>
 		</div>
-		<legend><i class="fas fa-table"></i> {{Mes Philips Hue}}</legend>
-		<input class="form-control" placeholder="{{Rechercher}}" id="in_searchEqlogic" />
+		<legend><i class="far fa-lightbulb"></i> {{Mes Philips Hue}}</legend>
+		<div class="input-group" style="margin:5px;">
+			<input class="form-control roundedLeft" placeholder="{{Rechercher}}" id="in_searchEqlogic"/>
+			<div class="input-group-btn">
+				<a id="bt_resetSearch" class="btn roundedRight" style="width:30px"><i class="fas fa-times"></i></a>
+			</div>
+		</div>
 		<div class="eqLogicThumbnailContainer">
 			<?php
 			foreach ($eqLogics as $eqLogic) {
@@ -49,154 +54,182 @@ $eqLogics = eqLogic::byType($plugin->getId());
 	<div class="col-xs-12 eqLogic" style="display: none;">
 		<div class="input-group pull-right" style="display:inline-flex">
 			<span class="input-group-btn">
-				<a class="btn btn-default eqLogicAction btn-sm roundedLeft" data-action="configure"><i class="fas fa-cogs"></i> {{Configuration avancée}}</a><a class="btn btn-sm btn-success eqLogicAction" data-action="save"><i class="fas fa-check-circle"></i> {{Sauvegarder}}</a><a class="btn btn-danger btn-sm eqLogicAction roundedRight" data-action="remove"><i class="fas fa-minus-circle"></i> {{Supprimer}}</a>
+				<a class="btn btn-default eqLogicAction btn-sm roundedLeft" data-action="configure"><i class="fas fa-cogs"></i> {{Configuration avancée}}
+				</a><a class="btn btn-sm btn-success eqLogicAction" data-action="save"><i class="fas fa-check-circle"></i> {{Sauvegarder}}
+				</a><a class="btn btn-danger btn-sm eqLogicAction roundedRight" data-action="remove"><i class="fas fa-minus-circle"></i> {{Supprimer}}</a>
 			</span>
 		</div>
-		
+
 		<ul class="nav nav-tabs" role="tablist">
 			<li role="presentation"><a class="eqLogicAction cursor" aria-controls="home" role="tab" data-action="returnToThumbnailDisplay"><i class="fas fa-arrow-circle-left"></i></a></li>
 			<li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fas fa-tachometer-alt"></i> {{Equipement}}</a></li>
 			<li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fas fa-list-alt"></i> {{Commandes}}</a></li>
 		</ul>
-		
-		<div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
+
+		<div class="tab-content">
 			<div role="tabpanel" class="tab-pane active" id="eqlogictab">
-				<br/>
-				<div class="row">
-					<div class="col-sm-6">
-						<form class="form-horizontal">
-							<fieldset>
-								<div class="form-group">
-									<label class="col-lg-4 control-label">{{Nom de l'équipement Philips Hue}}</label>
-									<div class="col-lg-6">
-										<input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
-										<input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement philipsHue}}"/>
-									</div>
+				<!-- <div class="row">
+				<div class="col-sm-6"> -->
+				<form class="form-horizontal">
+					<fieldset>
+						<div class="col-lg-6">
+							<legend><i class="fas fa-wrench"></i> {{Général}}</legend>
+							<div class="form-group">
+								<label class="col-sm-3 control-label">{{Nom de l'équipement}}</label>
+								<div class="col-sm-7">
+									<input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
+									<input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement philipsHue}}"/>
 								</div>
-								<div class="form-group">
-									<label class="col-lg-4 control-label" >{{Objet parent}}</label>
-									<div class="col-lg-6">
-										<select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
-											<option value="">{{Aucun}}</option>
-											<?php
-											$options = '';
-											foreach ((jeeObject::buildTree(null, false)) as $object) {
-												$options .= '<option value="' . $object->getId() . '">' . str_repeat('&nbsp;&nbsp;', $object->getConfiguration('parentNumber')) . $object->getName() . '</option>';
-											}
-											echo $options;
-											?>
-										</select>
-									</div>
-								</div>
-								<div class="form-group">
-									<label class="col-lg-4 control-label">{{Catégorie}}</label>
-									<div class="col-lg-8">
+							</div>
+							<div class="form-group">
+								<label class="col-sm-3 control-label" >{{Objet parent}}</label>
+								<div class="col-sm-7">
+									<select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
+										<option value="">{{Aucun}}</option>
 										<?php
-										foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
-											echo '<label class="checkbox-inline">';
-											echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" />' . $value['name'];
-											echo '</label>';
+										$options = '';
+										foreach ((jeeObject::buildTree(null, false)) as $object) {
+											$options .= '<option value="' . $object->getId() . '">' . str_repeat('&nbsp;&nbsp;', $object->getConfiguration('parentNumber')) . $object->getName() . '</option>';
+										}
+										echo $options;
+										?>
+									</select>
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-sm-3 control-label">{{Catégorie}}</label>
+								<div class="col-sm-7">
+									<?php
+									foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
+										echo '<label class="checkbox-inline">';
+										echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" />' . $value['name'];
+										echo '</label>';
+									}
+									?>
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-sm-3 control-label">{{Options}}</label>
+								<div class="col-sm-7">
+									<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
+									<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
+								</div>
+							</div>
+							<br>
+
+							<legend><i class="fas fa-cogs"></i> {{Paramètres}}</legend>
+							<div class="form-group">
+								<label class="col-sm-3 control-label">{{Modèle}}
+									<sup><i class="fas fa-question-circle tooltips" title="{{Sélectionner le modèle d'équipement Philips Hue à piloter}}"></i></sup>
+								</label>
+								<div class="col-sm-7">
+									<select class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="device">
+										<option value="">Aucun</option>
+										<?php
+										$groups = array();
+										foreach (philipsHue::devicesParameters() as $key => $info) {
+											echo '<option value="' . $key . '">[' . $key . '] ' . $info['name'] . '</option>';
 										}
 										?>
-									</div>
+									</select>
 								</div>
-								<div class="form-group">
-									<label class="col-lg-4 control-label"></label>
-									<div class="col-lg-8">
-										<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
-										<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
-									</div>
+							</div>
+							<div class="form-group">
+								<label class="col-sm-3 control-label">{{Pont}}
+									<sup><i class="fas fa-question-circle tooltips" title="{{Pont Philips Hue sur lequel l'équipement est connecté}}"></i></sup>
+								</label>
+								<div class="col-sm-7">
+									<select class="eqLogicAttr form-control" disabled data-l1key="configuration" data-l2key="bridge">
+										<?php
+										for($i=1;$i<=config::byKey('nbBridge','philipsHue');$i++){
+											echo '<option value="'.$i.'">{{Pont}} '.$i.'</option>';
+										}
+										?>
+									</select>
 								</div>
-								<div class="form-group">
-									<label class="col-lg-4 control-label">{{Pont}}</label>
-									<div class="col-lg-6">
-										<select class="eqLogicAttr form-control" disabled data-l1key="configuration" data-l2key="bridge">
-											<?php
-											for($i=1;$i<=config::byKey('nbBridge','philipsHue');$i++){
-												echo '<option value="'.$i.'">{{Pont}} '.$i.'</option>';
-											}
-											?>
-										</select>
-									</div>
+							</div>
+							<div class="form-group">
+								<label class="col-sm-3 control-label">{{Toujours allumé}}
+									<sup><i class="fas fa-question-circle tooltips" title="{{Cocher la case pour ne jamais indiquer l'équipement comme non joignable}}"></i></sup>
+								</label>
+								<div class="col-sm-7">
+									<input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="alwaysOn" />
 								</div>
-								<div class="form-group">
-									<label class="col-lg-4 control-label">{{Toujours allumé}}</label>
-									<div class="col-lg-2">
-										<input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="alwaysOn" />
-									</div>
-								</div>
-							</fieldset>
-						</form>
+							</div>
+							<!-- </fieldset>
+						</form> -->
 					</div>
-					<div class="col-sm-6">
-						<legend>{{Informations}}</legend>
-						<form class="form-horizontal">
-							<fieldset>
-								<div class="form-group">
-									<label class="col-sm-2 control-label">{{Modèle}}</label>
-									<div class="col-sm-8">
-										<select class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="device">
-											<option value="">Aucun</option>
-											<?php
-											$groups = array();
-											foreach (philipsHue::devicesParameters() as $key => $info) {
-												echo '<option value="' . $key . '">[' . $key . '] ' . $info['name'] . '</option>';
-											}
-											?>
-										</select>
-									</div>
-								</div>
-								<div class="form-group">
-									<label class="col-lg-3 control-label">{{Catégorie}}</label>
-									<div class="col-lg-2">
-										<span class="eqLogicAttr tooltips label label-default" data-l1key="configuration" data-l2key="category" style="font-size : 1em"></span>
-									</div>
-								</div>
-								<div class="form-group">
-									<label class="col-lg-3 control-label">{{Type}}</label>
-									<div class="col-lg-2">
-										<span class="eqLogicAttr tooltips label label-default" data-l1key="configuration" data-l2key="type" style="font-size : 1em"></span>
-									</div>
-								</div>
-								<div class="form-group">
-									<label class="col-lg-3 control-label">{{ID}}</label>
-									<div class="col-lg-2">
-										<span class="eqLogicAttr tooltips label label-default" data-l1key="configuration" data-l2key="id" style="font-size : 1em"></span>
-									</div>
-								</div>
-								<div class="form-group">
-									<label class="col-lg-3 control-label">{{Non model}}</label>
-									<div class="col-lg-2">
-										<span class="eqLogicAttr tooltips label label-default" data-l1key="configuration" data-l2key="modelName" style="font-size : 1em"></span>
-									</div>
-								</div>
-								<div class="form-group">
-									<label class="col-lg-3 control-label">{{Version du logiciel}}</label>
-									<div class="col-lg-2">
-										<span class="eqLogicAttr tooltips label label-default" data-l1key="configuration" data-l2key="softwareVersion" style="font-size : 1em"></span>
-									</div>
-								</div>
+
+					<div class="col-lg-6">
+						<legend><i class="fas fa-info"></i> {{Informations}}</legend>
+						<div class="form-group">
+							<label class="col-sm-3 control-label">{{Catégorie}}</label>
+							<div class="col-sm-7">
+								<span class="eqLogicAttr tooltips label label-default" data-l1key="configuration" data-l2key="category" style="font-size : 1em"></span>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-sm-3 control-label">{{Type}}</label>
+							<div class="col-sm-7">
+								<span class="eqLogicAttr tooltips label label-default" data-l1key="configuration" data-l2key="type" style="font-size : 1em"></span>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-sm-3 control-label">{{Identifiant}}</label>
+							<div class="col-sm-7">
+								<span class="eqLogicAttr tooltips label label-default" data-l1key="configuration" data-l2key="id" style="font-size : 1em"></span>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-sm-3 control-label">{{Nom du modèle}}</label>
+							<div class="col-sm-7">
+								<span class="eqLogicAttr tooltips label label-default" data-l1key="configuration" data-l2key="modelName" style="font-size : 1em"></span>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-sm-3 control-label">{{Version du logiciel}}</label>
+							<div class="col-sm-7">
+								<span class="eqLogicAttr tooltips label label-default" data-l1key="configuration" data-l2key="softwareVersion" style="font-size : 1em"></span>
+							</div>
+						</div>
+						<br>
+						<div class="form-group">
+							<label class="col-sm-3 control-label">{{Visuel}}</label>
+							<div class="col-sm-7">
 								<center>
-									<img src="<?php echo $plugin->getPathImgIcon() ?>" data-original=".jpg" id="img_device" class="img-responsive" style="max-height : 250px;"  onerror="this.src='<?php echo $plugin->getPathImgIcon() ?>'"/>
+									<img src="<?php echo $plugin->getPathImgIcon() ?>" data-original=".jpg" id="img_device" class="img-responsive" style="max-width:160px;"  onerror="this.src='<?php echo $plugin->getPathImgIcon() ?>'"/>
 								</center>
-							</fieldset>
-						</form>
+							</div>
+						</div>
 					</div>
-				</div>
-			</div>
-			<div role="tabpanel" class="tab-pane" id="commandtab">
+
+				</fieldset>
+			</form>
+			<hr>
+		</div>
+
+		<div role="tabpanel" class="tab-pane" id="commandtab">
+			<br/>
+			<div class="table-responsive">
 				<table id="table_cmd" class="table table-bordered table-condensed">
 					<thead>
 						<tr>
-							<th>{{Nom}}</th><th>{{Options}}</th><th>{{Action}}</th>
+							<!-- <th>{{Id}}</th> -->
+							<th>{{Nom}}</th>
+							<!-- <th>{{Type}}</th> -->
+							<th>{{Options}}</th>
+							<th>{{Paramètres}}</th>
+							<th>{{Action}}</th>
 						</tr>
 					</thead>
-					<tbody></tbody>
+					<tbody>
+					</tbody>
 				</table>
 			</div>
 		</div>
-		
 	</div>
+
+</div>
 </div>
 
 <?php include_file('desktop', 'philipsHue', 'js', 'philipsHue');?>

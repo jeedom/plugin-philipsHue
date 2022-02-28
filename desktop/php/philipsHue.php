0f25fdc64cf5acb939965707324875ab857e61e9
Loïc <loic@jeedom.com>
2022-02-28 09:00:54 +0100

    add table mode

--- desktop/php/philipsHue.php
+++ desktop/php/philipsHue.php
@@ -39,6 +39,7 @@ sendVarToJS('nb_philipsHue_bridge', config::byKey('nbBridge', 'philipsHue'));
 			<input class="form-control roundedLeft" placeholder="{{Rechercher}}" id="in_searchEqlogic" />
 			<div class="input-group-btn">
 				<a id="bt_resetSearch" class="btn roundedRight" style="width:30px"><i class="fas fa-times"></i></a>
+				<a class="btn roundedRight hidden" id="bt_pluginDisplayAsTable" data-coreSupport="1" data-state="0"><i class="fas fa-grip-lines"></i></a>
 			</div>
 		</div>
 		<div class="eqLogicThumbnailContainer">

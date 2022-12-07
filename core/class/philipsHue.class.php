3f48f79e8058d1c700bb82f39f3ac32bf552c35b
Loïc <loic@jeedom.com>
2022-12-07 14:23:20 +0100

    remove debug

--- core/class/philipsHue.class.php
+++ core/class/philipsHue.class.php
@@ -362,16 +362,6 @@ class philipsHue extends eqLogic {
 		}
 	}
 
-	public static function cron5() {
-		return;
-		for ($i = 1; $i <= config::byKey('nbBridge', 'philipsHue'); $i++) {
-			if (config::byKey('bridge_ip' . $i, 'philipsHue') == '') {
-				continue;
-			}
-			self::syncState($i);
-		}
-	}
-
 	public static function cron15() {
 		for ($i = 1; $i <= config::byKey('nbBridge', 'philipsHue'); $i++) {
 			if (config::byKey('bridge_ip' . $i, 'philipsHue') == '') {
@@ -402,7 +392,7 @@ class philipsHue extends eqLogic {
 			$hue = self::getPhilipsHue($_bridge_number);
 			$_datas = array_merge_recursive($hue->light(), $hue->motion(), $hue->temperature());
 		}
-		log::add('philipsHue', 'debug', 'Received message for bridge : ' . $_bridge_number . ' => ' . json_encode($_datas));
+		//log::add('philipsHue', 'debug', 'Received message for bridge : ' . $_bridge_number . ' => ' . json_encode($_datas));
 		foreach ($_datas['data'] as $data) {
 			if (!isset($data['owner']['rid'])) {
 				continue;

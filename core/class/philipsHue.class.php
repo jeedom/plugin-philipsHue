ec2ebbc6c29ded13b0f45d5aceb8fdd624625f33
Loïc <1536036+zoic21@users.noreply.github.com>
2024-01-10 15:56:37 +0100

    Update philipsHue.class.php

--- core/class/philipsHue.class.php
+++ core/class/philipsHue.class.php
@@ -465,9 +465,6 @@ class philipsHue extends eqLogic {
 
 		$scenes = $hue->scene();
 		foreach ($scenes['data'] as $scene) {
-			if (!isset($scene['group']['rtype']) || $scene['group']['rtype'] != 'room') {
-				continue;
-			}
 			$eqLogic = self::byLogicalId($scene['group']['rid'], 'philipsHue');
 			if (!is_object($eqLogic)) {
 				continue;
@@ -494,7 +491,7 @@ class philipsHue extends eqLogic {
 			$cmd->save();
 		}
       	
-      	$scenes = $hue->smart_scene();
+      		$scenes = $hue->smart_scene();
 		foreach ($scenes['data'] as $scene) {
 			$eqLogic = self::byLogicalId($scene['group']['rid'], 'philipsHue');
 			if (!is_object($eqLogic)) {
